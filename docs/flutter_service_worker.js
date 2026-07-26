bc216c9632b79efc85db26520d9bf93721552c6a
Jose Largacha <jose2557@users.noreply.github.com>
2026-07-26 08:46:57 -0500

    v2.7.2: ranking por periodo calculado desde el registro de puntos

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
-"flutter_bootstrap.js": "52ab4397c4f85b3958b0f0d51c9e983f",
+"flutter_bootstrap.js": "0d76cf49d5b92c089d7083d6e2c0c5cf",
 "icons/Icon-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "icons/Icon-512.png": "f941e25e98e2c8a8600d790815d4effd",
 "icons/Icon-maskable-512.png": "f941e25e98e2c8a8600d790815d4effd",
@@ -12,7 +12,7 @@ const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
 "icons/Icon-maskable-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "index.html": "90f9315308ebde790452e054f1b0e589",
 "/": "90f9315308ebde790452e054f1b0e589",
-"main.dart.js": "879a288f6e7014581b814c78df9a24d0",
+"main.dart.js": "a8b13635145ff973579660ba46165d09",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
