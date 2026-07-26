d1ca780308a5dd858f4cebe8efc3bcc194e90d9b
Jose Largacha <jose2557@users.noreply.github.com>
2026-07-26 08:19:08 -0500

    v2.7.1: ranking de hoy, margenes seguros, union manual, pagina de descarga con enlace permanente

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "426974928"
+    serviceWorkerVersion: "1425528123"
   }
 });
