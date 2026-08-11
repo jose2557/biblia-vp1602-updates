0054ea529e50a84db508b57bb446d89f8ca04c7d
jose2557 <joseangel.2557@gmail.com>
2026-08-11 08:24:47 -0500

    v2.12.20: las respuestas de Preguntas ya no aceptan un digito de mas

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
-"flutter_bootstrap.js": "8fccc07dc8dc5256ff1b74e9edebe0e9",
+"flutter_bootstrap.js": "a68d4b3f6f2ad7908e2ebe6579abd25a",
 "icons/Icon-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "icons/Icon-512.png": "f941e25e98e2c8a8600d790815d4effd",
 "icons/Icon-maskable-512.png": "f941e25e98e2c8a8600d790815d4effd",
@@ -12,7 +12,7 @@ const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
 "icons/Icon-maskable-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "index.html": "90f9315308ebde790452e054f1b0e589",
 "/": "90f9315308ebde790452e054f1b0e589",
-"main.dart.js": "8d3f709f234dc070f0229f78177340b6",
+"main.dart.js": "dad242e899e9f715943a574646ad95ff",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
