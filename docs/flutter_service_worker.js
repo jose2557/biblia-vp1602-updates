97605727c38b668e41c1a747316bfba20ed86ff1
jose2557 <joseangel.2557@gmail.com>
2026-08-11 06:16:50 -0500

    v2.12.19: rendimiento, records, puntos de la cita, preguntas falladas, insignias sin duplicar, Escudo a Preguntas

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
-"flutter_bootstrap.js": "c726cbe475f1659fe9e7c27048e68243",
+"flutter_bootstrap.js": "8fccc07dc8dc5256ff1b74e9edebe0e9",
 "icons/Icon-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "icons/Icon-512.png": "f941e25e98e2c8a8600d790815d4effd",
 "icons/Icon-maskable-512.png": "f941e25e98e2c8a8600d790815d4effd",
@@ -12,7 +12,7 @@ const RESOURCES = {"favicon.png": "79f6f78af13fb2fab68f055bddc0f7e4",
 "icons/Icon-maskable-192.png": "e22880caeda4817ee47b4ac105fedc8b",
 "index.html": "90f9315308ebde790452e054f1b0e589",
 "/": "90f9315308ebde790452e054f1b0e589",
-"main.dart.js": "fa5412c16b24bb5b96cc13ed71bed992",
+"main.dart.js": "8d3f709f234dc070f0229f78177340b6",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
