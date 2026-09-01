2345d38a58a31606279c9ff552646220ce3dbd46
Jose Largacha <joseangel.2557@gmail.com>
2026-09-01 13:45:10 -0500

    Publicar 2.21.1 (102): insignias de septiembre prematuras y Salón de la fe reparados

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -151,8 +151,8 @@ const RESOURCES = {"version.json": "1ba74e8f1021fb8f63b21ae6d52067f3",
 "assets/assets/biblia_vp1602.json": "85958e9aaacf55268ca1f1cfb427b1af",
 "assets/assets/memorizacion.json": "d1757e3fed8c0ef156f1b6a7ee6609e5",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"main.dart.js": "da0135f8406f74b7e69c7cb9187833a0",
-"flutter_bootstrap.js": "a9b0ff67477816b22ac6c3b7336382e6",
+"main.dart.js": "130459c416b424af67fe9f970f5403ce",
+"flutter_bootstrap.js": "6037b88b314ee9f9e38509905f9d9df5",
 "manifest.json": "b4780c526ffd9213e234a3d91cf12b40"};
 // The application shell files that are downloaded before a service worker can
 // start.
